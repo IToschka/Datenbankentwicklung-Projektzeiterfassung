26dadc6e984378277756da78571f6c48698d6545
IToschka <toschka@stud.dhbw-ravensburg.de>
2021-08-18 15:08:20 +0200

    html zu php ohne Inhalt

---
+++ workingTimeRecording.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="utf-8">
+       <title>Arbeitszeiterfassung</title>
+    </head>
+
+    <body>
+        <h1>Erfassungsbereich der Arbeitszeiten</h1>
+            <form>
+                <input type="text" name="Project" placeholder="Projekt 1">
+                <br>
+                <input type="text" name="Task" placeholder="Projektaufgabe 1">
+                <br>
+                <input type="time" name="BeginTime" placeholder="Von">
+                <br>
+                <input type="time" name="EndTime" placeholder="Bis">
+                <br>
+                <input type="submit" name="save" value="Speichern">
+            </form>
+    </body>
+</html>
