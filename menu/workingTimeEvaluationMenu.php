0e1bbf71435c60b867cce5c6f882c7f8c1c85597
Tamara Romer <tamara.romer1@gmx.de>
2021-08-30 17:11:08 +0200

    Auswertungen

---
+++ menu/workingTimeEvaluationMenu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="utf-8">
+       <link rel="stylesheet" href="../css/style.css">
+       <title></title>
+    </head>
+
+    <body>
+    <h1>Auswertung der Arbeitszeiten</h1>
+    <nav>
+    <ul>
+      <li><a href="../workingTimeEvaluation/evaluationTotal.php">Gesamtübersicht</a></li>
+      <li><a href="../workingTimeEvaluation/evaluationPerProject.php">Auswertung je Projekt</a></li>
+      <li><a href="../workingTimeEvaluation/evaluationPerEmployee.php">Auswertung je Mitarbeiter</a></li>
+    </ul>
+    </nav>
+
+    </body>
+</html>
