0c92205c122b5cf9c72140c0210812006fc10e23
Tamara Romer <tamara.romer1@gmx.de>
2021-09-16 17:28:38 +0200

    Datenbank

--- Zusätzliche Dateien/projecttimerecording.sql
+++ Zusätzliche Dateien/projecttimerecording.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 05. Sep 2021 um 15:42
+-- Erstellungszeit: 16. Sep 2021 um 16:52
 -- Server-Version: 10.4.19-MariaDB
 -- PHP-Version: 8.0.7
 
@@ -38,6 +38,22 @@ DECLARE MaxPnr MEDIUMINT(6);
       return MaxPnr;
   End$$
 
+CREATE DEFINER=`root`@`localhost` FUNCTION `GetAveragePerProject` (`sumInSec` INT, `projectId` INT) RETURNS INT(11) Begin
+DECLARE NumberOfValues INT;	
+DECLARE Average INT;
+	SELECT COUNT(PNR) INTO NumberOfValues FROM employeeproject WHERE ProjectID = projectId; 
+    SET Average = sumInSec / NumberOfValues;
+    return Average;      
+End$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `GetAverageTotal` (`sumInSec` INT) RETURNS INT(11) Begin
+DECLARE NumberOfValues INT;	
+DECLARE Average INT;
+	SELECT COUNT(PNR) INTO NumberOfValues FROM employee WHERE PNR != '0';
+    SET Average = sumInSec / NumberOfValues;
+    return Average;      
+End$$
+
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -64,10 +80,22 @@ CREATE TABLE `employee` (
 
 INSERT INTO `employee` (`PNR`, `Firstname`, `Lastname`, `CoreWorkingTimeFrom`, `CoreWorkingTimeTo`, `HiringDate`, `WeeklyWorkingHours`, `LastDateEntered`, `ProjectManager`) VALUES
 (0, 'Admin', '', '00:00:00', '00:00:00', '2000-01-01', 0, NULL, 1),
-(1, 'Max', 'Mustermann', '08:00:00', '16:00:00', '2002-06-06', 42, NULL, 1),
-(2, 'Annika', 'Arnold', '06:00:00', '15:00:00', '2010-08-22', 25, NULL, 0),
-(3, 'Bernd', 'Bader', '07:00:00', '16:00:00', '2003-07-27', 35, NULL, 1),
-(4, 'Daniela', 'Dolderer', '09:00:00', '17:00:00', '2011-08-02', 32, NULL, 0);
+(1, 'Max', 'Mustermann', '08:00:00', '16:00:00', '2002-06-06', 42, '2021-09-03', 1),
+(2, 'Annika', 'Arnold', '06:00:00', '15:00:00', '2010-08-22', 25, '2021-09-02', 0),
+(4, 'Daniela', 'Dolderer', '09:00:00', '17:00:00', '2011-08-02', 32, '2021-09-01', 0),
+(5, 'Carolin', 'Chu', '12:00:00', '16:00:00', '2021-08-01', 20, '2021-07-30', 1);
+
+--
+-- Trigger `employee`
+--
+DELIMITER $$
+CREATE TRIGGER `SetProjectManagerToAdmin` BEFORE DELETE ON `employee` FOR EACH ROW BEGIN
+
+UPDATE project SET ProjectManagerPNR = "0" WHERE ProjectManagerPNR = old.pnr;
+
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -85,7 +113,8 @@ CREATE TABLE `employeeproject` (
 --
 
 INSERT INTO `employeeproject` (`PNR`, `ProjectID`) VALUES
-(1, 1);
+(1, 1),
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -104,9 +133,9 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`PNR`, `Password`) VALUES
 (1, 'Hallo'),
-(2, 'IchhabekeinenPaln'),
-(3, 'Hallo'),
-(4, 'DHbw2019!');
+(2, 'IchhabekeinenPlan'),
+(4, 'DHbw2019!'),
+(5, 'Hallo');
 
 -- --------------------------------------------------------
 
@@ -126,11 +155,11 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`ProjectID`, `ProjectName`, `ProjectManagerPNR`, `BeginDate`) VALUES
-(1, 'Gebäudesanierung', 2, '2021-09-20'),
+(1, 'Gebäudesanierung', 2, '2021-09-02'),
 (2, 'S/4 HANA Conversion', 4, '2022-03-20'),
-(3, 'Entwicklung Bauteil X', 2, '2021-09-20'),
+(3, 'Entwicklung Bauteil X', 2, '2021-08-02'),
 (4, 'Testprojekt', 4, '2022-03-20'),
-(5, 'Softwareauswahl Einkaufsprozesse', 3, '2022-04-03');
+(5, 'Softwareauswahl Einkaufsprozesse', 0, '2021-06-07');
 
 -- --------------------------------------------------------
 
@@ -141,22 +170,25 @@ INSERT INTO `project` (`ProjectID`, `ProjectName`, `ProjectManagerPNR`, `BeginDa
 CREATE TABLE `projecttask` (
   `ProjectTaskID` int(11) NOT NULL,
   `ProjectID` mediumint(9) NOT NULL,
-  `Description` varchar(2000) NOT NULL
+  `TaskDescription` varchar(2000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Daten für Tabelle `projecttask`
 --
 
-INSERT INTO `projecttask` (`ProjectTaskID`, `ProjectID`, `Description`) VALUES
+INSERT INTO `projecttask` (`ProjectTaskID`, `ProjectID`, `TaskDescription`) VALUES
 (1, 1, 'Auswahl eines Anbieters, der unseren qualikativen Anforderungen gewachsen ist.'),
 (1, 2, 'Ansatz für die Konvertierung wählen.'),
 (1, 3, 'Zeichnung des Bauteils erstellen.'),
+(1, 5, 'Marktanalyse durchführen.'),
 (2, 1, 'Planung der Raumwechsel.'),
 (2, 2, 'Aufteilung der Zuständigkeitsbereiche.'),
 (2, 3, 'Technische Machbarkeit abklären und konkreten Bauplan erstellen.'),
-(3, 2, 'Schulungen durchführen für die Fachbereiche'),
+(2, 5, 'Anbeiter vergleichen.'),
+(3, 2, 'Schulungen durchführen für die Fachbereiche.'),
 (3, 3, 'Lieferantenauswahl und Einkauf der Bauteile.'),
+(3, 5, 'Nutzwertanalyse durchführen.'),
 (4, 3, 'Bauphase'),
 (5, 3, 'Testphase'),
 (6, 3, 'Übergang zur Serienproduktion managen.');
@@ -224,34 +256,7 @@ INSERT INTO `timerecording` (`PNR`, `ProjectID`, `ProjectTaskID`, `RecordingDate
 (1, 2, 2, '2021-09-08', '15:00:00', '16:00:00'),
 (1, 2, 2, '2021-09-13', '15:00:00', '16:00:00'),
 (1, 2, 2, '2021-09-14', '15:00:00', '16:00:00'),
-(2, 2, 1, '2021-09-06', '16:00:00', '17:00:00'),
-(3, 1, 1, '2021-09-06', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-07', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-08', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-13', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-14', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-15', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-16', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-17', '07:00:00', '08:00:00'),
-(3, 1, 1, '2021-09-20', '07:00:00', '08:00:00'),
-(3, 1, 2, '2021-09-06', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-07', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-08', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-13', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-14', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-15', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-16', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-17', '10:00:00', '11:00:00'),
-(3, 1, 2, '2021-09-20', '10:00:00', '11:00:00'),
-(3, 1, 3, '2021-09-06', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-07', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-08', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-13', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-14', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-15', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-16', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-17', '13:00:00', '14:00:00'),
-(3, 1, 3, '2021-09-20', '13:00:00', '14:00:00');
+(2, 2, 1, '2021-09-06', '16:00:00', '17:00:00');
 
 --
 -- Indizes der exportierten Tabellen
@@ -330,7 +335,7 @@ ALTER TABLE `login`
 -- Constraints der Tabelle `project`
 --
 ALTER TABLE `project`
-  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`ProjectManagerPNR`) REFERENCES `employee` (`PNR`);
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`ProjectManagerPNR`) REFERENCES `employee` (`PNR`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints der Tabelle `projecttask`
@@ -342,7 +347,7 @@ ALTER TABLE `projecttask`
 -- Constraints der Tabelle `timerecording`
 --
 ALTER TABLE `timerecording`
-  ADD CONSTRAINT `timerecording_ibfk_1` FOREIGN KEY (`PNR`) REFERENCES `employee` (`PNR`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `timerecording_ibfk_1` FOREIGN KEY (`PNR`) REFERENCES `employee` (`PNR`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `timerecording_ibfk_2` FOREIGN KEY (`ProjectTaskID`) REFERENCES `projecttask` (`ProjectTaskID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `timerecording_ibfk_3` FOREIGN KEY (`ProjectID`) REFERENCES `projecttask` (`ProjectID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
