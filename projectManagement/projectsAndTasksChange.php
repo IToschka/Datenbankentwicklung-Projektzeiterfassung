724d7e70c3738a8b386f4d884e57316606cd29f3
Tamara Romer <tamara.romer1@gmx.de>
2021-09-17 18:47:03 +0200

    Anlegen der Aufgaben

--- projectManagement/projectsAndTasksChange.php
+++ projectManagement/projectsAndTasksChange.php
@@ -1,7 +1,7 @@
 <?php
 //Autor: Katja Frei
     include_once '../menu/projectsAndTasksMenu.php';
-    
+
 
 ?>
 
@@ -17,31 +17,29 @@
     <body>
 
         <?php include_once '../includes/dbh.inc.php';
-                include_once 'includes/projectManagementFunctions.inc.php';
-        
+              include_once 'includes/projectManagementFunctions.inc.php';
+
         $error = "";
         $projectName = "";
         $projectManager = "";
+
         if(isset($_POST['button_choose'])) {
-            $projectID = $_POST['projectID']; 
+            $projectID = $_POST['projectID'];
             $_SESSION['projectID'] = $projectID;
             $projectManager = $_POST['projectManager'];
-         
-           /*  $error = noAccess($conn, $projectID, $projectManager);
-           /* header("location: ../projectsAndTasksChange.php?error=noAccess");
-            exit(); */
-          
-        
+
+
+
          if($error == "") {
-        $error = fillProject($conn, $projectID); 
+        $error = fillProject($conn, $projectID);
         $projectName = $_SESSION['projectName'];
         $beginDate = $_SESSION['beginDate'];
         }
     }
-                
+
                      ?>
         <form method="post" action="projectsAndTasksChange.php">
-        <p>ProjektID:<input type="number" name="projectID" required ></p>
+        <p>ProjektID:<input type="number" name="projectID" value= '<?php echo htmlentities($_SESSION['projectID']); ?>' required ></p>
             <input type="submit" name="button_choose" value="Bestätigen">
             <input type="submit" name="button_projectManagerMenu" value="Zurück zum Hauptmenü">
             </form>
@@ -58,20 +56,20 @@
                           <td>Starttermin:</td>
                           <td><input type="date" texarea readonly ="readonly" name="beginDate" value='<?php  echo $beginDate; ?>'></td>
                       </tr>
-                      
+
                       <tr>
                       <td>PNR Projektleiter:</td>
-                      <td><input type="text" texarea readonly ="readonly" name="projectManager" value= <?php $projectManager = $_SESSION['pnr']; echo $projectManager ?> ></td> 
-                      
+                      <td><input type="text" texarea readonly ="readonly" name="projectManager" value= <?php $projectManager = $_SESSION['pnr']; echo $projectManager ?> ></td>
+
                       </tr>
 
 
-                <?php 
+                <?php
         if(isset($_POST['button_addTask'])) {
             $projectID = $_SESSION['projectID'];
             //$_SESSION['projectID'] = $projectID;
-            
-            
+
+
             $projectTaskID = getTaskID($conn, $projectID);
             //$projectTaskID = $row['ProjectTaskID'];
             $id = $projectTaskID +1;
@@ -79,23 +77,33 @@
 
             echo '<tr><td>Aufgabe ' .$id. ':</td><td><textarea name="task"  maxlength="2000" cols="50"></textarea></td></tr>';
         }
+
+
+        if(isset($_POST['button_change'])) {
+            $task = $_POST['task'];
+            $projectTaskID = getTaskID($conn, $_SESSION['projectID']);
+            $projectTaskID++;
+            $projectID = $_SESSION['projectID'];
+
+            createTasks($conn, $projectTaskID, $projectID, $task);
+        }
                 ?>
-               
+
 
                   </tbody>
               </table>
             <input type="submit" name="button_addTask" value="Aufgabe hinzufügen">
             <input type="submit" name="button_change" value="Eingaben speichern">
-            
+
          </form>
          </body>
 
          <?php
 
-    
+
              if ($error == "invalidProjectID") {
                  echo "<p>Zur angegebenen Projekt ID besteht kein Projekt!</p>";
-             } 
+             }
             elseif ($error == "noAccess") {
                 echo "<p>Projektleiter dürfen nur ihre eingenen Projekte ändern!</p>";
             }
@@ -106,7 +114,7 @@
                  echo "<p>Das Projekt wurde erfolgreich geändert!</p>";
              }
 
-         
+
 
          ?>
 
